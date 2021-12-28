10eeba238ec8924a5ebde86b62b77b6356e291c4
emmanz95 <emmanza2@gmail.com>
2021-12-28 01:40:47 +0000

    refactored back to original

--- src/pages/income/IncomeView.jsx
+++ src/pages/income/IncomeView.jsx
@@ -9,60 +9,55 @@ import moment from 'moment';
 import useOpen from '../../hooks/useOpen';
 import { DropdownMenu, MenuItem } from 'react-bootstrap-dropdown-menu';
 import { toast, ToastContainer } from 'react-toastify';
-import { connect } from 'react-redux';
-import { fetchIncomes } from '../../app/actions/incomes';
-// import incomes from "../../app/reducers/incomes";
 
-function IncomeView(props) {
+function IncomeView() {
     const [openModal, setOpenModal] = useState(false);
-    // const [incomeData, setIncomeData] = useState([]);
+    const [incomeData, setIncomeData] = useState([]);
     const [loading, setLoading] = useState(false);
     const open = () => setOpenModal(true);
     const close = () => setOpenModal(false);
     useEffect(() => {
         setLoading(true)
-        // IncomeAPI.getIncome().then((results) => {
-        //     setIncomeData(results?.docs);
-        //     console.log(results);
-        //     setLoading(false);
-        // }).catch((error) => {
-        //     setLoading(false);
-        //     console.log(error);
-        // })
-        props.fetchIncomes().then((res) => {
-           setLoading(false);
-        }).catch((error) => {
-            console.log(error)
+        IncomeAPI.getIncome().then((results) => {
+            setIncomeData(results?.docs);
+            console.log(results);
             setLoading(false);
+        }).catch((error) => {
+            setLoading(false);
+            console.log(error);
+        })    }, [])
+    const handleDeleteIncome = (id) => {
+        IncomeAPI.onDeleteIncome(id).then((results) => {
+            if(results) {
+                console.log(results)
+                const deleteIncome = incomeData?.filter(doc => {
+                    return doc._id !== results._id;
+                })
+                setIncomeData(deleteIncome)
+                toast(`Successfully deleted id: ${results?._id}`)
+            } else {
+                return null;
+            }
+        }).catch((error) => {
+            console.log(error);
         })
-    }, [])
-    // const handleDeleteIncome = (id) => {
-    //     IncomeAPI.onDeleteIncome(id).then((results) => {
-    //         if(results) {
-    //             console.log(results)
-    //             const deleteIncome = incomeData?.filter(doc => {
-    //                 return doc._id !== results._id;
-    //             })
-    //             setIncomeData(deleteIncome)
-    //             toast(`Successfully deleted id: ${results?._id}`)
-    //         } else {
-    //             return null;
-    //         }
-    //     }).catch((error) => {
-    //         console.log(error);
-    //     })
-    // }
+    }
     const handleAddIncome = (formData) => {
         IncomeAPI.onCreateIncome(formData).then((results) => {
             toast('Successfully posted new Income!');
-            console.log(props?.incomes);
-            // setIncomeData([ ...incomeData, results ]);
+            console.log(incomeData);
+            setIncomeData([ ...incomeData, results ]);
             close()
         }, (error) => {
             console.log(error.response.data.msg);
         })
     }
     const { openClose, toggle } = useOpen;
+    const mapStateToProps = (state) => {
+        return {
+            incomes: state.incomes
+        }
+    }
     return (
         <div>
             <Navbar />
@@ -77,14 +72,14 @@ function IncomeView(props) {
                 <br />
                 {!loading ?
                     <div className="card-flex">
-                        {props?.incomes?.length > 0 ? props?.incomes?.map(income => (
+                        {incomeData?.length > 0 ? incomeData?.map(income => (
                             <Card key={income?.id}>
                                 <div className="header">
                                     <h4>{income?.title}</h4>
                                     {/*<i className="fas fa-ellipsis-v" onClick={() => toggle()} />*/}
                                     <DropdownMenu triggerType='icon' trigger='glyphicon glyphicon-option-vertical'>
                                         <MenuItem text={<i className="far fa-edit">{' '}Update</i>} />
-                                        {/*<MenuItem text={<i className="fas fa-trash" onClick={() => handleDeleteIncome(income?._id)}>{' '}Delete</i>} />*/}
+                                        <MenuItem text={<i className="fas fa-trash" onClick={() => handleDeleteIncome(income?._id)}>{' '}Delete</i>} />
                                     </DropdownMenu>
                                 </div><br />
                                 <p>{income?.description}</p>
@@ -110,12 +105,4 @@ function IncomeView(props) {
     );
 }
 
-const mapStateToProps = (state) => {
-    return {
-        incomes: state.incomes
-    }
-}
-
-export default connect(mapStateToProps, {
-    fetchIncomes
-})(IncomeView);
+export default IncomeView;
